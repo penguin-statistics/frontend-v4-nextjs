2709b0dae30c96a091abe8a4e749d63856a22cc3
GalvinGao <galvingaoper@gmail.com>
2021-09-16 16:54:05 +0800

    fix: import path

--- pages/result/item/index.tsx
+++ pages/result/item/index.tsx
@@ -1,6 +1,6 @@
 import {NextPage} from "next";
 import {useRouter} from "next/router";
-import styles from './/styles/Home.module.css'
+import styles from '../../../styles/Home.module.css'
 import {useItems} from "hooks/useItems";
 
 interface ItemIndexProps {}
